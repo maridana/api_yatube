014bc981651aa2fed467b60f1c9cd8b266cd9294
maridana <marina.krasnopevtceva@gmail.com>
2022-12-25 09:15:46 +0300

    Ревью2

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,8 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from rest_framework.authtoken import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
